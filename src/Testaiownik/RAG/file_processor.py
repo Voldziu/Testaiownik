78b74b203dd19bf1a76c4d5f87344f1a9f457a8b
Marcin Mleczak <marcinmmleczak@gmail.com>
2025-06-18 17:36:42 +0200

    add more metadata for PDF and PPTX

--- src/Testaiownik/RAG/file_processor.py
+++ src/Testaiownik/RAG/file_processor.py
@@ -1,17 +1,18 @@
+from typing import List
 from docx import Document
 import pdfplumber
 from utils.logger import logger
 from pptx import Presentation
 
-def extract_text_from_pdf(pdf_path: str) -> str:
-    """Extracts all text from a PDF file."""
+def extract_text_from_pdf(pdf_path: str) -> List[tuple[str, int]]:
+    """Extracts all text from a PDF file with page numbers."""
     with pdfplumber.open(pdf_path) as pdf:
-        text = ""
-        for page in pdf.pages:
+        results = []
+        for i, page in enumerate(pdf.pages, start=1):
             page_text = page.extract_text()
-            if page_text:  # Sprawdzamy, czy tekst nie jest None
-                text += page_text
-        return text
+            if page_text:
+                results.append((page_text, i))
+        return results
 
 def extract_text_from_txt(txt_path: str) -> str:
     """Extracts all text from a TXT file."""
@@ -23,19 +24,23 @@ def extract_text_from_txt(txt_path: str) -> str:
         logger.error(f"Error reading the text file: {e}")
         return ""
     
-def extract_text_from_pptx(pptx_path: str) -> str:
-        """Extracts text from a PPTX file."""
-        text = ""
-        try:
-            presentation = Presentation(pptx_path)
-            for slide in presentation.slides:
-                for shape in slide.shapes:
-                    if hasattr(shape, 'text'):  # Check if the shape has text
-                        text += shape.text + "\n"  # Add the text from the shape to the final text
-            return text.strip()
-        except Exception as e:
-            logger.error(f"Błąd podczas wczytywania pliku PPTX: {e}")
-            return ""
+def extract_text_from_pptx(pptx_path: str) -> List[tuple[str, int]]:
+    """Extracts text from a PPTX file with slide numbers."""
+    results = []
+    try:
+        presentation = Presentation(pptx_path)
+        for i, slide in enumerate(presentation.slides, start=1):
+            slide_text = ""
+            for shape in slide.shapes:
+                if hasattr(shape, 'text'):
+                    slide_text += shape.text + "\n"
+            if slide_text.strip():
+                results.append((slide_text.strip(), i))
+        return results
+    except Exception as e:
+        logger.error(f"Błąd podczas wczytywania pliku PPTX: {e}")
+        return []
+
         
 
 def extract_text_from_docx(docx_path: str) -> str:
